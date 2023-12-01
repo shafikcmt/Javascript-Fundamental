6d03dd20ed222e1efe95280d25c0916a97689c6a
shafikcmt <mdshafiqulislam822@gmail.com>
2023-12-01 18:04:21 +0530

    Javascript Basic and Variable completed

---
+++ main.js
@@ -0,0 +1,35 @@
+document.write('Hello HTML');
+
+// Javascript Output
+document.getElementById('test').innerHTML = 'Hello Geeta University';
+// window.alert('Hello India');
+console.log('Hello PHP')
+
+// Javascript Variable
+// var , let , const
+
+var a = '<br> Geeta University';
+var a = '<br> Hello PHP';
+    a = 54;
+document.write('<br>'+a);
+document.write('<hr>');
+let b = 34;
+// let b = 89;
+    b = 39;
+document.write(b);
+document.write('<hr>');
+const c = 50;
+// const c = 78;
+    // c = 67;
+if(a==54){
+    var a = 45;
+    let x = 32;
+    document.write('Value with let Inside'+ x);
+    const y = 78;
+}
+document.write('Value with var'+ a);
+document.write('Value with let'+ x);
+document.write('Value with const'+ x);
+// document.write(c);
+
+
